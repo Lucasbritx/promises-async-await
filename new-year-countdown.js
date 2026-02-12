46a35711883351503957eb3c8fa0d47b4e6430b4
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-02-12 00:26:34 -0300

    JS is single thread

---
+++ new-year-countdown.js
@@ -0,0 +1,19 @@
+function newYearsCountdown() {
+  print("3");
+  sleep(1000);
+
+  print("2");
+  sleep(1000);
+
+  print("1");
+  sleep(1000);
+
+  print("Happy New Year! 🎉");
+}
+
+/* 
+It doesn't exists because there is no sleep function in JS, it's a single thread language. 
+JS can do only one thing at a time
+
+
+*/
