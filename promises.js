7ffd69f5b749142d71907c1afafb09f16e69c8f4
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-02-12 00:45:10 -0300

    working with promises

---
+++ promises.js
@@ -0,0 +1,30 @@
+/* Promises: better way to async calls instead nesting. */
+// It will not work, setTimeout uses older callback style
+/* console.log("3");
+
+setTimeout(1000)
+  .then(() => {
+    console.log("2");
+
+    return setTimeout(1000);
+  })
+  .then(() => {
+    console.log("1");
+
+    return setTimeout(1000);
+  })
+  .then(() => {
+    console.log("Happy New Year!!");
+  }); */
+
+// fecthing = will return promises
+const fetchValue = fetch("/api/get-data");
+
+console.log(fetchValue);
+// -> Promise {<pending>}
+
+/* promise states:
+pending: WIP
+fulfilled: completed
+rejected: error
+*/
