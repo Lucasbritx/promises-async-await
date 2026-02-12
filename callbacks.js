879fbb6640016e85d0401a670ac398ab641c849e
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-02-12 00:31:50 -0300

    async code doesn't run linear

--- callbacks.js
+++ callbacks.js
@@ -6,3 +6,15 @@ console.log("Start");
 setTimeout(() => {
   console.log("After one second");
 }, 1000);
+
+/* Async code will not run linear */
+console.log("1. Before setTimeout");
+
+setTimeout(() => {
+  console.log("2. Inside setTimeout");
+}, 500);
+
+console.log("3. After setTimeout");
+/* output: 1. Before setTimeout
+3. After setTimeout
+2. Inside setTimeout */
